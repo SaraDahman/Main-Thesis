c46eaed90d2dfa4604f12a90d82aa01b05c7b481
RulaAbuHasna <rulahasna99@gmail.com>
2020-08-04 10:31:17 +0300

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Image from './Componants/UploadImage';
+import SignUp from './Componants/signUp-business/signUp';
 
 function App() {
   return (
     <div className='App'>
-      <Image />
+      <SignUp />
     </div>
   );
 }
