d732696a3a75df5b73710347568948cf01742c28
unknown <saritadh74@gmail.com>
2020-08-09 11:07:36 +0300

    addMeal done

--- client/src/Componants/restaurant/res.js
+++ client/src/Componants/restaurant/res.js
@@ -1,24 +1,24 @@
-import React from "react";
-import img from "../Pictures/0.jpg";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import Uploadimage from "../UploadImage";
-var jwtDecode = require("jwt-decode");
+import React from 'react';
+import img from '../Pictures/0.jpg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Uploadimage from '../UploadImage';
+var jwtDecode = require('jwt-decode');
 // import jwtDecode from "jwt-decode";
 
 function Res() {
   // localStorage.getItem("tokenIdBusiness");
-  var token = localStorage.getItem("tokenIdBusiness");
+  var token = localStorage.getItem('tokenIdBusiness');
   // var decoded = jwtDecode(token);
 
   var idBusiness = token;
-  console.log(token);
+  console.log('=====token===>', token);
   const [counter, setCounter] = useState(0);
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [amount, setAmount] = useState("");
-  const [price, setPrice] = useState("");
-  const [pic, setPic] = useState("");
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState('');
+  const [price, setPrice] = useState('');
+  const [pic, setPic] = useState('');
   const [meals, setMeals] = useState([]);
 
   useEffect(() => {
@@ -62,79 +62,117 @@ function Res() {
     console.log(pic);
   };
 
-  return (
-    <div className="con">
-      <div className="addmeal" id="add">
-        <h1>ADD MEAL</h1>
-        <input
-          type="text"
-          placeholder="NAME"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        ></input>
-        <br />
-        <br />
-        <input
-          type="text"
-          placeholder="DESCRIPTION"
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-        ></input>
-        <br />
-        <br />
-        <input
-          type="number"
-          placeholder="AMOUNT"
-          value={amount}
-          onChange={(event) => setAmount(event.target.value)}
-        ></input>
-        <br />
-        <br />
-        <input
-          type="text"
-          placeholder="price"
-          value={price}
-          onChange={(event) => setPrice(event.target.value)}
-        ></input>
-        <br />
-        <Uploadimage imgurl={imgCallback} />
-        <br />
-        <br />
-        <button onClick={handleSubmit}> Add </button>
-      </div>
+  if (meals) {
+    return (
+      <div className='con'>
+        <div className='addmeal' id='add'>
+          <h1>ADD MEAL</h1>
+          <input
+            type='text'
+            placeholder='NAME'
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          ></input>
+          <br />
+          <br />
+          <input
+            type='text'
+            placeholder='DESCRIPTION'
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          ></input>
+          <br />
+          <br />
+          <input
+            type='number'
+            placeholder='AMOUNT'
+            value={amount}
+            onChange={(event) => setAmount(event.target.value)}
+          ></input>
+          <br />
+          <br />
+          <input
+            type='text'
+            placeholder='price'
+            value={price}
+            onChange={(event) => setPrice(event.target.value)}
+          ></input>
+          <br />
+          <Uploadimage imgurl={imgCallback} />
+          <br />
+          <br />
+          <button onClick={handleSubmit}> Add </button>
+        </div>
 
-      {/* meeeaaalllss */}
-      <div className="addmeal" id="cards">
-        {meals.map((Element, index) => {
-          return (
-            <div className="card" key={index}>
-              <img src={Element.image} alt="Avatar" style={{ width: "100%" }} />
-              <div className="container1">
-                <h4>
-                  <b>{Element.mealName}</b>
-                </h4>
-                <p>{Element.mealAmount}</p>
-                <p className='p'>{Element.discription}</p>
-                <br />
-                <button>Delete</button>
+        {/* meeeaaalllss */}
+        <div className='addmeal' id='cards'>
+          {meals.map((Element, index) => {
+            return (
+              <div className='card' key={index}>
+                <img
+                  src={Element.image}
+                  alt='Avatar'
+                  style={{ width: '100%' }}
+                />
+                <div className='container1'>
+                  <h4>
+                    <b>{Element.mealName}</b>
+                  </h4>
+                  <p>{Element.mealAmount}</p>
+                  <p className='p'>{Element.discription}</p>
+                  <br />
+                  <button>Delete</button>
+                </div>
               </div>
-            </div>
-          );
-        })}
-        {/*  . . .  */}
-        <br />
-        {/* <div className='card'>
-          <img src={img} alt='Avatar' style={{ width: '100%' }} />
-          <div className='container1'>
-            <h4>
-              <b>Name</b>
-            </h4>
-            <p className='p'>Description</p>
-          </div>
-        </div> */}
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className='con'>
+        <div className='addmeal' id='add'>
+          <h1>ADD MEAL</h1>
+          <input
+            type='text'
+            placeholder='NAME'
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          ></input>
+          <br />
+          <br />
+          <input
+            type='text'
+            placeholder='DESCRIPTION'
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          ></input>
+          <br />
+          <br />
+          <input
+            type='number'
+            placeholder='AMOUNT'
+            value={amount}
+            onChange={(event) => setAmount(event.target.value)}
+          ></input>
+          <br />
+          <br />
+          <input
+            type='text'
+            placeholder='price'
+            value={price}
+            onChange={(event) => setPrice(event.target.value)}
+          ></input>
+          <br />
+          <Uploadimage imgurl={imgCallback} />
+          <br />
+          <br />
+          <button onClick={handleSubmit}> Add </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Res;
