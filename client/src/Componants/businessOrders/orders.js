5a89576fb90cbb9b0cabe20d10d00183d72716ab
unknown <saritadh74@gmail.com>
2020-08-14 21:57:18 +0300

    orders

--- client/src/Componants/businessOrders/orders.js
+++ client/src/Componants/businessOrders/orders.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+// import ItemsCarousel from "react-items-carousel";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 var token = localStorage.getItem('tokenIdBusiness');
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    width: 270,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+});
+
 function Orders() {
+  const classes = useStyles();
+
   const [counter, setCounter] = useState(0);
   const [orders, setOrders] = useState([]);
   var [clientss, setClients] = useState({});
@@ -38,7 +60,7 @@ function Orders() {
             obj.orders = clientss[key];
             x.push(obj);
           }
-          
+
           setOrders(x);
           console.log(x);
         })
@@ -48,7 +70,7 @@ function Orders() {
     }
 
     rendering();
-  }, [orders, counter]);
+  }, [counter]);
 
   let decrement = (e) => {
     var mealId = e.target.name;
@@ -68,9 +90,48 @@ function Orders() {
   };
 
   return (
-    <div className='addmeal' id='cards'>
+    <div>
       <h1>ORDERS</h1>
-      <p>{orders.length}</p>
+      {orders.map((Element, index) => {
+        return (
+          <div key={index} className='orders'>
+            <div>
+              <h3>
+                Client: &nbsp;
+                {Element.name}
+              </h3>
+              <h3>
+                Phone Number: &nbsp;
+                {Element.phone}
+              </h3>
+            </div>
+            <div className='orders'>
+              {Element.orders.map((Element2, index2) => {
+                return (
+                  <div className='card1' key={index2}>
+                    <img
+                      src={Element2.image}
+                      alt='Avatar'
+                      style={{ width: '100%', height: '240px' }}
+                    />
+                    <div className='container1'>
+                      <h4>
+                        <b>{Element2.mealName}</b>
+                        <br />
+                        <span>{Element2.mealAmount}</span>
+                      </h4>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <Button variant='contained' id='btn'>
+              Confirm
+            </Button>
+            <hr />
+          </div>
+        );
+      })}
     </div>
   );
 }
