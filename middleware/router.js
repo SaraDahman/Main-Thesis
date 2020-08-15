0b410fa50822f2b924692131c0607a2e6ef5e85d
NasrShamalkh <naaaa9sr-@hotmail.com>
2020-08-15 19:23:42 +0300

    stripePayment component

--- middleware/router.js
+++ middleware/router.js
@@ -2,7 +2,7 @@ var app = require('express').Router();
 const controller = require('./controller');
 
 app.route('/').get((req, res) => {
-	res.send('This is Good');
+  res.send('This is Good');
 });
 
 app.route('/user/signup').post(controller.addUser);
@@ -14,16 +14,16 @@ app.route('/login').post(controller.login);
 app.route('/logout').post(controller.logout);
 
 app
-	.route('/user/login/:userId')
-	.post(controller.findUser)
-	.get(controller.findUserById);
+  .route('/user/login/:userId')
+  .post(controller.findUser)
+  .get(controller.findUserById);
 
 app.route('/order/add/:userId').post(controller.addOrderUser);
 
 app
-	.route('/order/remove/:userId')
-	.post(controller.removeOrderUser)
-	.put(controller.removeBusOrderUser);
+  .route('/order/remove/:userId')
+  .post(controller.removeOrderUser)
+  .put(controller.removeBusOrderUser);
 
 app.route('/order/find/:userId').get(controller.findOrderUser);
 
@@ -36,28 +36,32 @@ app.route('/business').get(controller.findAllBusiness);
 app.route('/business/:idBusiness').post(controller.findBusiness);
 
 app.route('/meal/add/:idBusiness').post(controller.addMealToBusiness);
-//nasr 
+//nasr
 app.route('/confirmEmail').post(controller.confirmEmail);
 app.route('/emailConfirmation/:userId').get(controller.emailConfirmation);
 //----
+//-------- Payment -------
+app.route('/stripeCheckout').get(controller.stripeCheckoutGet);
+app.route('/stripeCheckout').post(controller.stripeCheckoutPost);
+
+//------------------------
+app
+  .route('/meal/pending/:idBusiness')
+  .post(controller.PendingMealToBusiness)
+  .get(controller.findMealInBusinessPending);
 
 app
-	.route('/meal/pending/:idBusiness')
-	.post(controller.PendingMealToBusiness)
-	.get(controller.findMealInBusinessPending);
+  .route('/meal/done/:idBusiness')
+  .post(controller.doneMealToBusiness)
+  .get(controller.findMealInBusinessDone);
 
 app
-	.route('/meal/done/:idBusiness')
-	.post(controller.doneMealToBusiness)
-	.get(controller.findMealInBusinessDone);
+  .route('/business/meal/pending/:idBusiness')
+  .post(controller.removePendinngMealInBusiness);
 
 app
-	.route('/business/meal/pending/:idBusiness')
-	.post(controller.removePendinngMealInBusiness);
-
-app
-	.route('/business/meal/pendingOne/:idBusiness')
-	.post(controller.PendinngMealInBusiness);
+  .route('/business/meal/pendingOne/:idBusiness')
+  .post(controller.PendinngMealInBusiness);
 
 app.route('/business/meal/:idBusiness').get(controller.findMealInBusiness);
 
