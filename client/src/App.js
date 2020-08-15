08470e5e394a5d0a795775f974b561195bcf69ce
unknown <saritadh74@gmail.com>
2020-08-15 09:52:36 +0300

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import './App.css';
-import Image from './Componants/UploadImage';
-import SignUp from './Componants/signUp-business/signUp';
-import SignupClient from './Componants/Sign-up-client/signup-client';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Landing from './Componants/landing/landing';
-import Nav from './Componants/navbar/navbar';
-import SignIn from './Componants/SignIn/SignIn';
-import Res from './Componants/restaurant/res';
-import Carousel from './Componants/carousel/carousel';
-import Menu from './Componants/menu/menu';
-import payment from './Componants/payment/payout';
-import Orders from './Componants/businessOrders/orders';
-import Order from './Componants/ordered/ordered';
-import User from './Componants/userpage/userpage';
-import Notify from './Componants/notifyTest/notification';
-=======
 import React from "react";
 import "./App.css";
 import Image from "./Componants/UploadImage";
@@ -33,7 +14,6 @@ import payment from "./Componants/payment/payout";
 import Orders from "./Componants/businessOrders/orders";
 import Order from "./Componants/ordered/ordered";
 import User from "./Componants/userpage/userpage";
->>>>>>> c1a34b8d1749b0ea9db62b254dff78a16d801575
 
 function App() {
   return (
@@ -41,21 +21,6 @@ function App() {
       <div className='App'>
         <Nav />
         <Switch>
-<<<<<<< HEAD
-          <Route exact path='/menu' component={Menu} />
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/sign-upClient' component={SignupClient} />
-          <Route exact path='/sign-upBusiness' component={SignUp} />
-          <Route exact path='/sign-in' component={SignIn} />
-          <Route exact path='/res' component={Res} />
-          <Route exact path='/upload' component={Image} />
-          <Route exact path='/c' component={Carousel} />
-          <Route exact path='/payment' component={payment} />
-          <Route exact path='/orders' component={Orders} />
-          <Route exact path='/order' component={Order} />
-          <Route exact path='/user' component={User} />
-          <Route exact path='/notify' component={Notify} />
-=======
           <Route exact path="/menu" component={Menu} />
           <Route exact path="/" component={Landing} />
           <Route exact path="/sign-upClient" component={SignupClient} />
@@ -68,7 +33,6 @@ function App() {
           <Route exact path="/orders" component={Orders} />
           <Route exact path="/order" component={Order} />
           <Route exact path="/user" component={User} />
->>>>>>> c1a34b8d1749b0ea9db62b254dff78a16d801575
         </Switch>
       </div>
     </Router>
