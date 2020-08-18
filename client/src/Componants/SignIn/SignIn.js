f5207c93ec70940212c3e9acba1d1c5c3b07b729
Mohammed Hourani <m.hourani98@gmail.com>
2020-08-18 13:59:46 +0300

    fix the map in sign in

--- client/src/Componants/SignIn/SignIn.js
+++ client/src/Componants/SignIn/SignIn.js
@@ -1,178 +1,151 @@
-import React, { useState, useEffect } from 'react';
-import Avatar from '@material-ui/core/Avatar';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
-import Map from './userMap';
-
 function Copyright() {
-  return (
-    <Typography variant='body2' color='textSecondary' align='center'>
-      {'Copyright © '}
-      <Link color='inherit' href='https://material-ui.com/'>
-        Side Menu
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+	return (
+		<Typography variant='body2' color='textSecondary' align='center'>
+			{'Copyright © '}
+			<Link color='inherit' href='https://material-ui.com/'>
+				Side Menu
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	);
 }
-
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+	paper: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(1),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
 }));
-
 export default function SignIn() {
-  const history = useHistory();
-  const classes = useStyles();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  let handleChange = (e) => {
-    if (e.target.name === 'email') {
-      setEmail(e.target.value);
-    } else if (e.target.name === 'password') {
-      setPassword(e.target.value);
-    }
-  };
-
-  var checkPassword = (e) => {
-    e.preventDefault();
-    console.log('success', email, password);
-    axios
-      .post('/login', {
-        email: email,
-        password: password,
-      })
-      .then((response) => {
-        console.log('success');
-        // if(response.data.confirmed) {
-
-        // console.log(response.data);
-        var token = response.data.token;
-        console.log(response.data);
-        // alert(response.data, "------ response.data ---- ")
-        var decoded = jwtDecode(token);
-        if (decoded.userId) {
-          localStorage.setItem('tokenIdBusiness', decoded.userId);
-          // window.location.reload("/menu");
-          // history.push("/menu");
-          window.location.href = '/user';
-        } else if (decoded.idBusiness) {
-          localStorage.setItem('tokenIdBusiness', decoded.idBusiness);
-          // window.location.reload();
-          window.location.href = '/res';
-          // history.push("/res");
-        }
-        // }else {
-        //   alert("please confirm your Email")
-        // }
-
-        //   alert(response.data);
-        // history.push("/res");
-      })
-      .catch((err) => {
-        console.log('err signing in!', err);
-      });
-  };
-
-  return (
-    <div>
-      <Map />
-      <Container component='main' maxWidth='xs'>
-        {/* <CssBaseline /> */}
-        <div className={classes.paper}>
-          {/* <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography> */}
-          <form className={classes.form} noValidate>
-            <TextField
-              variant='outlined'
-              margin='normal'
-              required
-              fullWidth
-              id='email'
-              label='Email Address'
-              name='email'
-              autoComplete='email'
-              autoFocus
-              onChange={(e) => handleChange(e)}
-            />
-            <TextField
-              variant='outlined'
-              margin='normal'
-              required
-              fullWidth
-              name='password'
-              label='Password'
-              type='password'
-              id='password'
-              autoComplete='current-password'
-              onChange={(e) => handleChange(e)}
-            />
-            <FormControlLabel
-              control={<Checkbox value='remember' color='primary' />}
-              label='Remember me'
-            />
-            <Button
-              type='submit'
-              fullWidth
-              variant='contained'
-              id='btn'
-              className={classes.submit}
-              onClick={(e) => checkPassword(e)}
-              href='/menu'
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href='#' variant='body2'>
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href='#' variant='body2'>
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-        <Box mt={8}>
-          <Copyright />
-        </Box>
-      </Container>
-    </div>
-  );
+	const classes = useStyles();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	let handleChange = (e) => {
+		if (e.target.name === 'email') {
+			setEmail(e.target.value);
+		} else if (e.target.name === 'password') {
+			setPassword(e.target.value);
+		}
+	};
+	var checkPassword = (e) => {
+		e.preventDefault();
+		navigator.geolocation.getCurrentPosition((position) => {
+			localStorage.setItem('poslatitude', position.coords.latitude);
+			localStorage.setItem('poslongitude', position.coords.longitude);
+		});
+		var location = localStorage.getItem('poslatitude');
+		if (location.length > 0) {
+			axios
+				.post('/login', {
+					email: email,
+					password: password,
+				})
+				.then((response) => {
+					var token = response.data.token;
+					var decoded = jwtDecode(token);
+					if (decoded.userId) {
+						localStorage.setItem('tokenIdBusiness', decoded.userId);
+						window.location.href = '/user';
+					} else if (decoded.idBusiness) {
+						localStorage.setItem('tokenIdBusiness', decoded.idBusiness);
+						window.location.href = '/res';
+					}
+				})
+				.catch((err) => {
+					console.log('err signing in!', err);
+				});
+		} else {
+			alert('choose your location');
+		}
+	};
+	return (
+		<div>
+			<Container component='main' maxWidth='xs'>
+				<div className={classes.paper}>
+					<form className={classes.form} noValidate>
+						<TextField
+							variant='outlined'
+							margin='normal'
+							required
+							fullWidth
+							id='email'
+							label='Email Address'
+							name='email'
+							autoComplete='email'
+							autoFocus
+							onChange={(e) => handleChange(e)}
+						/>
+						<TextField
+							variant='outlined'
+							margin='normal'
+							required
+							fullWidth
+							name='password'
+							label='Password'
+							type='password'
+							id='password'
+							autoComplete='current-password'
+							onChange={(e) => handleChange(e)}
+						/>
+						<FormControlLabel
+							control={<Checkbox value='remember' color='primary' />}
+							label='Remember me'
+						/>
+						<Button
+							type='submit'
+							fullWidth
+							variant='contained'
+							id='btn'
+							className={classes.submit}
+							onClick={(e) => checkPassword(e)}
+							href='/menu'
+						>
+							Sign In
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link href='#' variant='body2'>
+									Forgot password?
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link href='#' variant='body2'>
+									{"Don't have an account? Sign Up"}
+								</Link>
+							</Grid>
+						</Grid>
+					</form>
+				</div>
+				<Box mt={8}>
+					<Copyright />
+				</Box>
+			</Container>
+		</div>
+	);
 }
--- client/src/Componants/signUp-business/signUp.js
+++ client/src/Componants/signUp-business/signUp.js
@@ -12,209 +12,209 @@ import { useHistory } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: '350px',
-    },
-  },
+	root: {
+		'& > *': {
+			margin: theme.spacing(1),
+			width: '350px',
+		},
+	},
 }));
 
 function SignUp() {
-  const classes = useStyles();
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [phone, setPhone] = useState('');
-  const [type, setType] = useState('');
-  const [location, setLocation] = useState({});
-  const [BusinessImage, setBusinessImage] = useState('');
-  const [warning, setWarning] = useState('');
-  const [warning2, setWarning2] = useState('');
+	const classes = useStyles();
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [phone, setPhone] = useState('');
+	const [type, setType] = useState('');
+	const [location, setLocation] = useState({});
+	const [BusinessImage, setBusinessImage] = useState('');
+	const [warning, setWarning] = useState('');
+	const [warning2, setWarning2] = useState('');
 
-  // const [imageUrlm, setImageUrl] = useState("");
-  // const [imageAlt, setImageAlt] = useState("");
-  //, email:'',password:'',phone:'',restPhone:'',location:''}
-  //useEffect(() => {}, []);
+	// const [imageUrlm, setImageUrl] = useState("");
+	// const [imageAlt, setImageAlt] = useState("");
+	//, email:'',password:'',phone:'',restPhone:'',location:''}
+	//useEffect(() => {}, []);
 
-  let handleChange = (e) => {
-    // let input = e.target.value;
-    if (e.target.name === 'name') {
-      setName(e.target.value);
-    } else if (e.target.name === 'email') {
-      setEmail(e.target.value);
-    } else if (e.target.name === 'phone') {
-      if (
-        e.target.value.length > 7 &&
-        e.target.value.length < 14 &&
-        e.target.value.length !== 0
-      ) {
-        setPhone(e.target.value);
-        setWarning2('');
-      } else if (e.target.value.length !== 0) {
-        if (e.target.value.length < 7) {
-          setPhone(e.target.value);
-          setWarning2('it has to be larger than 7');
-        } else {
-          setPhone(e.target.value);
-          setWarning2('it has to be smaller than 14');
-        }
-      } else {
-        setPhone(e.target.value);
-        setWarning2('');
-      }
-    } else if (e.target.name === 'type') {
-      setType(e.target.value);
-    } else if (e.target.name === 'password') {
-      if (e.target.value.length < 8 && e.target.value.length !== 0) {
-        setPassword(e.target.value);
-        setWarning('it has to be greater than 8');
-      } else {
-        setWarning('');
-        setPassword(e.target.value);
-      }
-    } else if (e.target.name === 'BusinessImage') {
-      setBusinessImage(e.target.value);
-    }
-  };
-  const history = useHistory();
+	let handleChange = (e) => {
+		// let input = e.target.value;
+		if (e.target.name === 'name') {
+			setName(e.target.value);
+		} else if (e.target.name === 'email') {
+			setEmail(e.target.value);
+		} else if (e.target.name === 'phone') {
+			if (
+				e.target.value.length > 7 &&
+				e.target.value.length < 14 &&
+				e.target.value.length !== 0
+			) {
+				setPhone(e.target.value);
+				setWarning2('');
+			} else if (e.target.value.length !== 0) {
+				if (e.target.value.length < 7) {
+					setPhone(e.target.value);
+					setWarning2('it has to be larger than 7');
+				} else {
+					setPhone(e.target.value);
+					setWarning2('it has to be smaller than 14');
+				}
+			} else {
+				setPhone(e.target.value);
+				setWarning2('');
+			}
+		} else if (e.target.name === 'type') {
+			setType(e.target.value);
+		} else if (e.target.name === 'password') {
+			if (e.target.value.length < 8 && e.target.value.length !== 0) {
+				setPassword(e.target.value);
+				setWarning('it has to be greater than 8');
+			} else {
+				setWarning('');
+				setPassword(e.target.value);
+			}
+		} else if (e.target.name === 'BusinessImage') {
+			setBusinessImage(e.target.value);
+		}
+	};
+	const history = useHistory();
 
-  let handleSubmit = (e) => {
-    e.preventDefault();
-    //get business location
-    navigator.geolocation.getCurrentPosition(function (position) {
-      /* setLocation({
+	let handleSubmit = (e) => {
+		e.preventDefault();
+		//get business location
+		navigator.geolocation.getCurrentPosition(function (position) {
+			/* setLocation({
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       });*/
-      const location = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      };
-      console.log(location);
-      const phoneNum = Number(phone);
-      console.log(phoneNum, typeof phoneNum, ' ---- phoneNum ------');
-      axios
-        .post('/business/signup', {
-          BusinessName: name,
-          email: email,
-          password: password,
-          phone: Number(phone), // static phone number because post request doesnt work
-          location: location,
-          type: type,
-          BusinessImage: BusinessImage,
-        })
-        .then((response) => {
-          console.log('success');
-          console.log(response.data);
-          Swal.fire('User created successfully !!');
-          Swal.fire('please confirm your email to be able to sign in');
+			const location = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude,
+			};
+			console.log(location);
+			const phoneNum = Number(phone);
+			console.log(phoneNum, typeof phoneNum, ' ---- phoneNum ------');
+			axios
+				.post('/business/signup', {
+					BusinessName: name,
+					email: email,
+					password: password,
+					phone: Number(phone), // static phone number because post request doesnt work
+					location: location,
+					type: type,
+					BusinessImage: BusinessImage,
+				})
+				.then((response) => {
+					console.log('success');
+					console.log(response.data);
+					Swal.fire('User created successfully !!');
+					Swal.fire('please confirm your email to be able to sign in');
 
-          // alert("User created successfully !!", "an Email has been sent to your account, please confirm your email to be able to sign in !");
-          // const id = "" + response.data
-          // setUserId(id);
-          // console.log(userId, "------- user id -----")
-          // alert(userId);
-          axios
-            .post(`/confirmEmail`, {
-              userId: response.data,
-              email: email,
-            })
-            .then(() => {
-              console.log('confirmEmail is sent');
-            });
-          //   alert(response.data);
-          localStorage.setItem('singup', 'singup');
-          history.push('/sign-in');
-        })
-        .catch((err) => {
-          console.log('err signing in!', err);
-        });
-      console.log('Latitude is :', position.coords.latitude);
-      console.log('Longitude is :', position.coords.longitude);
-    });
+					// alert("User created successfully !!", "an Email has been sent to your account, please confirm your email to be able to sign in !");
+					// const id = "" + response.data
+					// setUserId(id);
+					// console.log(userId, "------- user id -----")
+					// alert(userId);
+					axios
+						.post(`/confirmEmail`, {
+							userId: response.data,
+							email: email,
+						})
+						.then(() => {
+							console.log('confirmEmail is sent');
+						});
+					//   alert(response.data);
+					localStorage.setItem('singup', 'singup');
+					history.push('/sign-in');
+				})
+				.catch((err) => {
+					console.log('err signing in!', err);
+				});
+			console.log('Latitude is :', position.coords.latitude);
+			console.log('Longitude is :', position.coords.longitude);
+		});
 
-    setBusinessImage('');
-    setName('');
-    setEmail('');
-    setLocation('');
-    setPassword('');
-    setPhone('');
-    setType('');
-  };
+		setBusinessImage('');
+		setName('');
+		setEmail('');
+		setLocation('');
+		setPassword('');
+		setPhone('');
+		setType('');
+	};
 
-  return (
-    <div className='container1'>
-      <div className='in'>
-        {location.lat}
-        <div className='form'>
-          <form
-            className={classes.root}
-            id='form'
-            noValidate
-            autoComplete='off'
-          >
-            <h1>Sign Up</h1>
-            <TextField
-              id='standard-basic'
-              label='Business name'
-              type='name'
-              name='name'
-              value={name}
-              onChange={(e) => handleChange(e)}
-            />{' '}
-            <br></br>
-            <TextField
-              id='standard-basic'
-              label='Business email'
-              type='email'
-              name='email'
-              value={email}
-              onChange={(e) => handleChange(e)}
-            />
-            <br></br>
-            <TextField
-              id='standard-basic'
-              label='Password'
-              type='password'
-              name='password'
-              value={password}
-              onChange={(e) => handleChange(e)}
-            />
-            <h6>{warning}</h6>
-            <br></br>
-            <TextField
-              id='standard-basic'
-              label='Business phone'
-              type='name'
-              name='phone'
-              value={phone}
-              onChange={(e) => handleChange(e)}
-            />
-            <h6>{warning2}</h6>
-            <br></br>
-            <TextField
-              id='standard-basic'
-              label='type'
-              type='name'
-              name='type'
-              value={type}
-              onChange={(e) => handleChange(e)}
-            />
-            <br></br>
-            <TextField
-              id='standard-basic'
-              label='Business Image'
-              type='text'
-              name='BusinessImage'
-              value={BusinessImage}
-              onChange={(e) => handleChange(e)}
-            />
-            {/* <div className="form-group">
+	return (
+		<div className='container1'>
+			<div className='in'>
+				{location.lat}
+				<div className='form'>
+					<form
+						className={classes.root}
+						id='form'
+						noValidate
+						autoComplete='off'
+					>
+						<h1>Sign Up</h1>
+						<TextField
+							id='standard-basic'
+							label='Business name'
+							type='name'
+							name='name'
+							value={name}
+							onChange={(e) => handleChange(e)}
+						/>{' '}
+						<br></br>
+						<TextField
+							id='standard-basic'
+							label='Business email'
+							type='email'
+							name='email'
+							value={email}
+							onChange={(e) => handleChange(e)}
+						/>
+						<br></br>
+						<TextField
+							id='standard-basic'
+							label='Password'
+							type='password'
+							name='password'
+							value={password}
+							onChange={(e) => handleChange(e)}
+						/>
+						<h6>{warning}</h6>
+						<br></br>
+						<TextField
+							id='standard-basic'
+							label='Business phone'
+							type='name'
+							name='phone'
+							value={phone}
+							onChange={(e) => handleChange(e)}
+						/>
+						<h6>{warning2}</h6>
+						<br></br>
+						<TextField
+							id='standard-basic'
+							label='type'
+							type='name'
+							name='type'
+							value={type}
+							onChange={(e) => handleChange(e)}
+						/>
+						<br></br>
+						<TextField
+							id='standard-basic'
+							label='Business Image'
+							type='text'
+							name='BusinessImage'
+							value={BusinessImage}
+							onChange={(e) => handleChange(e)}
+						/>
+						{/* <div className="form-group">
               <input type="file" />
             </div>
             <br></br> */}
-            {/* <TextField
+						{/* <TextField
               id="standard-basic"
               label="Location"
               type="name"
@@ -222,19 +222,19 @@ function SignUp() {
               value={location}
               onChange={(e) => handleChange(e)}
             /> */}
-            <br></br>
-            <br></br>
-            <Button variant='contained' id='btn' onClick={handleSubmit}>
-              Sign Up
-            </Button>
-          </form>
-        </div>
-      </div>
-      <div className='in'>
-        <img src={image} alt='' className='img'></img>
-      </div>
-    </div>
-  );
+						<br></br>
+						<br></br>
+						<Button variant='contained' id='btn' onClick={handleSubmit}>
+							Sign Up
+						</Button>
+					</form>
+				</div>
+			</div>
+			<div className='in'>
+				<img src={image} alt='' className='img'></img>
+			</div>
+		</div>
+	);
 }
 
 export default SignUp;
