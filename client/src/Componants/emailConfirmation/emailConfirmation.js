e9f635184eae1840cc10811833ba34971335a3b6
NasrShamalkh <naaaa9sr-@hotmail.com>
2020-08-21 22:59:43 +0300

    localhost => herokuapp.com

--- client/src/Componants/emailConfirmation/emailConfirmation.js
+++ client/src/Componants/emailConfirmation/emailConfirmation.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import axios from 'axios';
 class EmailConfirmation extends React.Component {
-	state = {};
-	componentDidMount() {
-		const { userId } = this.props.match.params;
-		axios
-			.get(`/emailConfirmation/:${userId}`)
-			.then(() => {
-				console.log('userId sent successfully');
-			})
-			.catch((err) => {
-				console.log('Error in updating status');
-			});
-	}
-	render() {
-		return (
-			<div>
-				<h2>Email confirmation</h2>
-				<p>your email has been confimed </p>
-				<br></br>
-				<a href='/sign-in'>Sign in ?</a>
-			</div>
-		);
-	}
+  state = {};
+  componentDidMount() {
+    const { userId } = this.props.match.params;
+    axios
+      .get(`/emailConfirmation/:${userId}`)
+      .then(() => {
+        console.log('userId sent successfully');
+      })
+      .catch(err => {
+        console.log('Error in updating status');
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Email confirmation</h2>
+        <p>your email has been confimed </p>
+        <br></br>
+        <a href='/sign-in'>Sign in ?</a>
+      </div>
+    );
+  }
 }
 export default EmailConfirmation;
