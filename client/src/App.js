59c2e8a8c2679ea8e153ca7b84027a656979b6d9
Mohammed Hourani <m.hourani98@gmail.com>
2020-08-21 14:18:20 +0300

    the last version

--- client/src/App.js
+++ client/src/App.js
@@ -14,40 +14,42 @@ import Order from './Componants/ordered/ordered';
 import User from './Componants/userpage/userpage';
 import Notify from './Componants/notifyTest/notification';
 import EmailConfirmation from './Componants/emailConfirmation/emailConfirmation';
+
 //----------- private route ---------//
 import {
-  BusinessPrivateRoute,
-  UserPrivateRoute,
-  SignIn,
+	BusinessPrivateRoute,
+	UserPrivateRoute,
+	SignIn,
 } from './Componants/SignIn/SignIn';
+
 //--------- private route ---------------//
 function App() {
-  return (
-    <Router>
-      <div className='App'>
-        <Nav />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/sign-upClient' component={SignupClient} />
-          <Route exact path='/sign-upBusiness' component={SignUp} />
-          <Route exact path='/sign-in' component={SignIn} />
-          <UserPrivateRoute exact path='/menu' component={Menu} />
-          <BusinessPrivateRoute exact path='/res' component={Res} />
-          <UserPrivateRoute exact path='/upload' component={Image} />
-          <UserPrivateRoute exact path='/payment' component={payment} />
-          <BusinessPrivateRoute exact path='/orders' component={Orders} />
-          <UserPrivateRoute exact path='/order' component={Order} />
-          <UserPrivateRoute exact path='/userpage' component={User} />
-          <Route exact path='/notify' component={Notify} />
-          <Route
-            exact
-            path='/emailConfirmation/:userId'
-            component={EmailConfirmation}
-          />
-        </Switch>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<Nav />
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/sign-upClient' component={SignupClient} />
+					<Route exact path='/sign-upBusiness' component={SignUp} />
+					<Route exact path='/sign-in' component={SignIn} />
+					<UserPrivateRoute exact path='/menu' component={Menu} />
+					<BusinessPrivateRoute exact path='/res' component={Res} />
+					<UserPrivateRoute exact path='/upload' component={Image} />
+					<UserPrivateRoute exact path='/payment' component={payment} />
+					<BusinessPrivateRoute exact path='/orders' component={Orders} />
+					<UserPrivateRoute exact path='/order' component={Order} />
+					<UserPrivateRoute exact path='/userpage' component={User} />
+					<Route exact path='/notify' component={Notify} />
+					<Route
+						exact
+						path='/emailConfirmation/:userId'
+						component={EmailConfirmation}
+					/>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
