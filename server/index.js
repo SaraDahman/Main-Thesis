37580286ed414d257d278b1a305b7b5bb9247c80
Mohammed Hourani <m.hourani98@gmail.com>
2020-08-21 19:02:58 +0300

    fix the .env file and put it in heroku directly

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -10,59 +11,58 @@ app.use(express.json());
 
 // connect to database
 mongoose
-  .connect(keys.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log('Database is connected to digital Page');
-  })
-  .catch((error) => {
-    console.log('Error In Database Connection');
-  });
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		console.log('Database is connected to digital Page');
+	})
+	.catch((error) => {
+		console.log('Error In Database Connection');
+	});
 
 const nexmo = new Nexmo({
-  apiKey: '14f4ccbf',
-  apiSecret: 'fK6BCU9FA8M3dLbu',
+	apiKey: '14f4ccbf',
+	apiSecret: 'fK6BCU9FA8M3dLbu',
 });
 
 app.post('/message', (req, res) => {
-  const { message, number } = req.body;
+	const { message, number } = req.body;
 
-  nexmo.message.sendSms(
-    'Nexmo',
-    Number(number),
-    message,
-    {
-      type: 'unicode',
-    },
-    (err, responseData) => {
-      if (err) {
-        console.log(err);
-      } else {
-        if (responseData.messages[0]['status'] === '0') {
-          console.log('Message sent successfully.');
-        } else {
-          console.log(
-            `Message failed with error: ${responseData.messages[0]['error-text']}`
-          );
-        }
-      }
-    }
-  );
-})
-
+	nexmo.message.sendSms(
+		'Nexmo',
+		Number(number),
+		message,
+		{
+			type: 'unicode',
+		},
+		(err, responseData) => {
+			if (err) {
+				console.log(err);
+			} else {
+				if (responseData.messages[0]['status'] === '0') {
+					console.log('Message sent successfully.');
+				} else {
+					console.log(
+						`Message failed with error: ${responseData.messages[0]['error-text']}`
+					);
+				}
+			}
+		}
+	);
+});
 
 app.use('/', links);
 
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-  });
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+	});
 }
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
